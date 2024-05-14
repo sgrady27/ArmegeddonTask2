640f16f1234267db7df503576d4e03bae756a26b
format27 <83480842+sgrady27@users.noreply.github.com>
2024-05-14 16:04:20 -0500

    task2 accurate

--- main.tf
+++ main.tf
@@ -1,57 +1,61 @@
-# create a europe vpc network
-resource "google_compute_network" "eu-vpc" {
-  name                    = var.eu-vpc.vpc.name
-  auto_create_subnetworks = false
-}
+# Create a publically accessible web page with Terraform.  You must complete the following
+# 1) Terraform Script with a VPC
+# 2) Terraform script must have a VM within your VPC.
+# 3) The VM must have the homepage on it.
+# 4) The VM must have an publically accessible link to it.
+# 5) You must Git Push your script to your Github.
+# 6) Outpub file must show 1) Public IP, 2) VPC, 3) Subnet of the VM, 4) Internal IP of the VM.
 
-# create an eu subnet
-resource "google_compute_subnetwork" "eu-subnet" {
-  name                     = var.eu-vpc.eu-subnet.name
-  ip_cidr_range            = var.eu-vpc.eu-subnet.cidr
-  region                   = var.eu-vpc.eu-subnet.region
-  network                  = google_compute_network.eu-vpc.id
-  private_ip_google_access = true
-}
 
-# create a firewall to allow http traffic in europe
-resource "google_compute_firewall" "eu-firewall" {
-  name    = var.eu-vpc.firewall
-  network = google_compute_network.eu-vpc.id
+# add compute instance to the VPC
+ resource "google_compute_instance" "task2" {
+    name         = var.instance_name
+    machine_type = var.machine_type
+    zone         = var.zone
 
-  allow {
-    protocol = "tcp"
-    ports    = var.ports[0]
-  }
 
-  source_ranges = [var.eu-vpc.eu-subnet.cidr, var.us-vpc.us-east-subnet.cidr,
-  var.us-vpc.us-west-subnet.cidr, var.asia-vpc.asia-subnet.cidr]
-}
-
-resource "google_compute_instance" "eu-instance" {
-  name         = var.eu-vpc.subnet.instance_name
-  machine_type = var.machine_types.linux.machine_type
-  zone         = var.eu-vpc.subnet.zone
 
   boot_disk {
     initialize_params {
-      image = var.machine_types.linux.image
+      image = "debian-cloud/debian-12"
       size  = 10
     }
+    # true by default
     auto_delete = true
   }
 
   network_interface {
-    network    = google_compute_network.eu-vpc.id
-    subnetwork = google_compute_subnetwork.eu-subnet.id
+    network    = var.network_name
+    subnetwork = var.subnet_name
 
-    # No public IP allowed
-    # access_config {
-    #   // Ephemeral public IP
-    # }
+    access_config {
+      // Ephemeral public IP
+    }
   }
 
-  tags = ["eu-http-server"]
+  tags = ["http-server"]
+
+  # metadata = {
+  #   startup-script-url = "${file("startup.sh")}"
+  # }
 
   metadata_startup_script = file("startup.sh")
 
+  # metadata_startup_script = <<-EOF
+  #   #!/bin/bash
+  #   apt-get update
+  #   apt-get install -y apache2
+  #   cat <<EOT > /var/www/html/index.html
+  #   <html>
+  #     <head>
+  #       <title>Welcome to My Homepage</title>
+  #     </head>
+  #     <body>
+  #       <h1>Welcome to My Homepage!</h1>
+  #       <p>This page is served by Apache on a Google Compute Engine VM instance.</p>
+  #     </body>
+  #   </html>
+  #   EOT
+  # EOF
 }
+
